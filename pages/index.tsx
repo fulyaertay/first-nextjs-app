afe2f94cbdf855830281f33ebc5ddb17bc7cf8ab
Fulya <fulya.ertay@gmail.com>
2023-01-18 15:19:02 +0300

    added back links

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
        <h1>Hello World! You are on home page</h1>
        <Link href="/">Home</Link>
        <Link href="./about/about">About</Link>

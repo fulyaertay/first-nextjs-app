d7d06cf152ae9312eae8004f7e07418696c7f370
Fulya <fulya.ertay@gmail.com>
2023-01-18 15:10:36 +0300

    added link component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -15,7 +15,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-       <h1>Hello World!</h1>
+       <h1>Hello World! You are on home page</h1>
+       <Link href="/">Home</Link>
+       <Link href="./about/about">About</Link>
+       <Link href="./posts/first-post">Posts</Link>
       </main>
     </>
   )

8dde9ee2a391fe6f7705f4f86f8a604c9664c674
Fulya <fulya.ertay@gmail.com>
2023-01-18 20:30:54 +0300

    added layout

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/global.css';
-import Header  from '../components/header'
-export default function App({ Component, pageProps }) {
-    return 
-    <Header/>
-    <Component {...pageProps} />;
-   
-  }
